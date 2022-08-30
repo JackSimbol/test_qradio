860864cd7fc03fd43f9575fcdcedea4838fd74bb
Xingbo Wang <91768006+JackSimbol@users.noreply.github.com>
2022-08-30 14:30:31 +0800

    Add files via upload

---
+++ radio.js
@@ -0,0 +1,38 @@
+/* 单选题交互js
+ * tid: 题干id
+ * oid：选项id(sid)的序列
+ * sid: 单个选项id(响应选择的radio)
+ */
+function check_radio(tid, sid){ //选择单选题某一选项时触发
+    document.getElementById(sid).attr("checked", true);
+    var oid = get_id(document.getElementById(tid).getAttribute("idlist"));
+    for(var id of oid){
+        iid = document.getElementById(id).getAttribute("in_id");
+        if(iid !== sid){
+            document.getElementById(iid).attr("checked", false);
+        }
+    }
+}
+
+/*
+ * 选择题文本示例：
+ * "id=abc,type=radio,body=[Who is the most beautiful woman?],options=[A.Queen][B.Snowwhite],checked=abc_a"
+ * 选项id表示为 选择题id+"_"+选项序号(A,B,C,...)
+ */
+
+// tid、body、type已预先在总函数中执行
+function unparse_radio(tid){ //将单选题结果处理成文本
+    var oid = get_id(document.getElementById(tid).getAttribute("idlist"));
+    var checked = "";
+    var opt_body = "options=";
+    for(var id of oid){ //oid内包含：input与text(选项内容)
+        var in_id = document.getElementById(id).getAttribute("in_id");
+        var ob_id = document.getElementById(id).getAttribute("ob_id");
+        opt_body += "["+document.getElementById(ob_id).innerHTML + "]";
+        if(document.getElementById(in_id).is(":checked")){
+            checked = id;
+        }
+    }
+    return opt_body+","+checked;
+}
+
